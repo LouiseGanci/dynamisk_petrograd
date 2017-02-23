01f4cbe19bc225085bf947e043fa47f9bfcfbf31
Louise Ganci Clausen <LouiseGanciClausen@kennethpc.kea.local>
2017-02-23 10:49:08 +0100

    Load JSON produkt and create template clone for each

--- petrograd.js
+++ petrograd.js
@@ -3,14 +3,27 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("Siden vises");
     visProdukt();
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
 }
 
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+var firstProduct = true
 
 function visProdukt(produkt) {
-    console.log("Produkt vises");
+    console.log(produkt);
 
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+    if (firstProduct == true) {
+        klon.querySelector(".knap_i_midten").classList.add(".col-md-offset-2");
+        firstProduct = false;
+    }
 
     //indsæt data i klon
 
