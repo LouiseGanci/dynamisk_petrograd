ac3c28b9f31a959ef7b486d623ca81bcada4de7b
Louise Ganci Clausen <LouiseGanciClausen@kennethpc.kea.local>
2017-02-23 10:37:26 +0100

    Add JavaScript template produkt

--- petrograd.js
+++ petrograd.js
@@ -2,18 +2,18 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden vises");
-
-    // læs produktliste
-    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+    visProdukt();
 }
 
-function visProduktListe(listen) {
-    console.table(listen);
-    listen.forEach(visProdukt);
-
-}
 
 function visProdukt(produkt) {
     console.log("Produkt vises");
-    var klon = document.querySelector("#produkt_template").content.clodeNode
+
+    //klon produkt_template
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
+    //indsæt data i klon
+
+    //append klon til .produkt_liste
+    document.querySelector(".produktliste").appendChild(klon);
 }
