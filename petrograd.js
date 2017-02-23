a024a267b75d1e1b8023598df0f8702b8a165022
Louise Ganci Clausen <LouiseGanciClausen@kennethpc.kea.local>
2017-02-23 11:03:04 +0100

    Add data (navn, pris, rabatpris)

--- petrograd.js
+++ petrograd.js
@@ -2,7 +2,6 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("Siden vises");
-    visProdukt();
 
     // læs produktliste
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
@@ -26,6 +25,12 @@ function visProdukt(produkt) {
     }
 
     //indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
+
 
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
